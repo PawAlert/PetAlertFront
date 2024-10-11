db3881fbf78ddd2c60c36212ec6d2a6f2bad3ccc
devji <dev.jinsung1017@gmail.com>
2024-10-12 08:38:49 +0900

    주소 변경

--- src/features/auth/api/LoginUser.ts
+++ src/features/auth/api/LoginUser.ts
@@ -4,7 +4,7 @@ import {useAuthStore} from '../model/store';
 import {LoginUserModel, SuccessLogin} from "../model/LoginUserModel.ts";
 
 const loginUserFn = async (credentials: LoginUserModel): Promise<SuccessLogin> => {
-    const response = await axios.post<SuccessLogin>('http://localhost:8080/api/nonMember/user/login', credentials);
+    const response = await axios.post<SuccessLogin>('https://port-0-pawalertbackendteamgroup-m06zwfj8628a2164.sel4.cloudtype.app/api/nonMember/user/login', credentials);
     return response.data;
 };
 
