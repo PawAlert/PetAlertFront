e6f74765c51aa221980cfbe0823041684377b9bf
devji <dev.jinsung1017@gmail.com>
2024-10-21 08:19:27 +0900

    에러 해결

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -4,18 +4,15 @@ import '../../index.css';
 import { RouterProvider } from "react-router-dom";
 import withRouter from "./routers/withRouter.tsx";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import TokenHandler from './TokenHandler'; // TokenHandler 컴포넌트를 import
 
 const queryClient = new QueryClient();
 
-const Root = () => (
+
+
+createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <TokenHandler>
-                <RouterProvider router={withRouter} />
-            </TokenHandler>
+            <RouterProvider router={withRouter} />
         </QueryClientProvider>
     </StrictMode>
 );
-
-createRoot(document.getElementById('root')!).render(<Root />);
