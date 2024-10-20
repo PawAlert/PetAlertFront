c78f0442a24f32b7c9917de0bad049e51603d187
devji <dev.jinsung1017@gmail.com>
2024-10-21 08:24:44 +0900

    에러 해결

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,18 +1,36 @@
-import { StrictMode } from 'react';
+import { StrictMode, useEffect } from 'react';
 import { createRoot } from 'react-dom/client';
 import '../../index.css';
 import { RouterProvider } from "react-router-dom";
 import withRouter from "./routers/withRouter.tsx";
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useAuth } from "../features/auth/Login/customHook/useAuth.ts";
 
 const queryClient = new QueryClient();
 
+function App() {
+    const { login } = useAuth();
 
+    useEffect(() => {
+        const searchParams = new URLSearchParams(window.location.search);
+        const token = searchParams.get('token');
+        if (token) {
+            console.log('Token detected:', token);
+            login(token);
+            // URL에서 토큰 제거
+            window.history.replaceState({}, document.title, window.location.pathname);
+        }
+    }, [login]);
+
+    return (
+        <RouterProvider router={withRouter} />
+    );
+}
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
         <QueryClientProvider client={queryClient}>
-            <RouterProvider router={withRouter} />
+            <App />
         </QueryClientProvider>
     </StrictMode>
 );
