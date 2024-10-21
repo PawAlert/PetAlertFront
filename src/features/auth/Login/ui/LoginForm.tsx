2b9104a50af86ee6573eece0abe45b9101918c94
devji <dev.jinsung1017@gmail.com>
2024-10-21 13:08:24 +0900

    에러 해결

--- src/features/auth/Login/ui/LoginForm.tsx
+++ src/features/auth/Login/ui/LoginForm.tsx
@@ -111,12 +111,12 @@ export const LoginForm: React.FC = () => {
                     >
                         Google
                     </button>
-                    <button
-                        onClick={() => handleSocialLogin('NAVER')}
-                        className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
-                    >
-                        Naver
-                    </button>
+                    {/*<button*/}
+                    {/*    onClick={() => handleSocialLogin('NAVER')}*/}
+                    {/*    className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"*/}
+                    {/*>*/}
+                    {/*    Naver*/}
+                    {/*</button>*/}
                     <button
                         onClick={() => handleSocialLogin('KAKAO')}
                         className="w-full inline-flex justify-center py-2 px-4 border border-gray-300 rounded-md shadow-sm bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
